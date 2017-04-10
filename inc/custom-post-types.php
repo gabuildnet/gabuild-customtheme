808ee715842d23deeb7d58c3950b2c0a2c4f1f35
Ghislain Arseneau <ghislain.arseneau@gmail.com>
2017-04-09 21:34:39 -0300

    added custom fields directly in php instead of db

--- inc/custom-post-types.php
+++ inc/custom-post-types.php
@@ -61,4 +61,112 @@ function gabuild_create_post_types() {
 		)
 	);
 }
+
+if(function_exists("register_field_group"))
+{
+  register_field_group(array (
+    'id' => 'acf_stories-additionnal-fields',
+    'title' => 'Stories Additionnal fields',
+    'fields' => array (
+      array (
+        'key' => 'field_584a0a435e266',
+        'label' => 'Story Project',
+        'name' => 'story_project',
+        'type' => 'relationship',
+        'required' => 1,
+        'return_format' => 'object',
+        'post_type' => array (
+          0 => 'projects',
+        ),
+        'taxonomy' => array (
+          0 => 'all',
+        ),
+        'filters' => array (
+          0 => 'search',
+        ),
+        'result_elements' => array (
+          0 => 'post_type',
+          1 => 'post_title',
+        ),
+        'max' => 1,
+      ),
+    ),
+    'location' => array (
+      array (
+        array (
+          'param' => 'post_type',
+          'operator' => '==',
+          'value' => 'stories',
+          'order_no' => 0,
+          'group_no' => 0,
+        ),
+      ),
+    ),
+    'options' => array (
+      'position' => 'normal',
+      'layout' => 'no_box',
+      'hide_on_screen' => array (
+      ),
+    ),
+    'menu_order' => 0,
+  ));
+  register_field_group(array (
+    'id' => 'acf_version-additional-fields',
+    'title' => 'Version Additional fields',
+    'fields' => array (
+      array (
+        'key' => 'field_584a061fa532b',
+        'label' => 'Version Project',
+        'name' => 'version_project',
+        'type' => 'relationship',
+        'required' => 1,
+        'return_format' => 'object',
+        'post_type' => array (
+          0 => 'projects',
+        ),
+        'taxonomy' => array (
+          0 => 'all',
+        ),
+        'filters' => array (
+          0 => 'search',
+        ),
+        'result_elements' => array (
+          0 => 'post_type',
+          1 => 'post_title',
+        ),
+        'max' => 1,
+      ),
+      array (
+        'key' => 'field_584a08f5a50fc',
+        'label' => 'BOM list',
+        'name' => 'bom_list',
+        'type' => 'wysiwyg',
+        'default_value' => '',
+        'toolbar' => 'full',
+        'media_upload' => 'yes',
+      ),
+    ),
+    'location' => array (
+      array (
+        array (
+          'param' => 'post_type',
+          'operator' => '==',
+          'value' => 'versions',
+          'order_no' => 0,
+          'group_no' => 0,
+        ),
+      ),
+    ),
+    'options' => array (
+      'position' => 'normal',
+      'layout' => 'default',
+      'hide_on_screen' => array (
+        0 => 'featured_image',
+      ),
+    ),
+    'menu_order' => 0,
+  ));
+}
+
+
 ?>
