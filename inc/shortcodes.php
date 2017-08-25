58c9c5d54bc1d63729e355134257c7158fbf4f8a
Ghislain Arseneau <ghislain.arseneau@gmail.com>
2017-08-25 07:01:40 -0300

    Removed side bar from front page and started creating shortcodes.

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -8,4 +8,31 @@ function gabuild_code_shortcode ($atts, $content = null) {
 }
 add_shortcode('gacode', 'gabuild_code_shortcode');
 
+function gabuild_projects_shortcode($atts, $content = null) {
+	$result = '';
+
+	$args = array (
+		'post_type' => 'projects',
+		'post_status' => 'publish',
+		'post_per_page' => 8,
+		'orderby' => 'date',
+		'order' => 'DESC'
+	);
+
+	$the_query = new WP_Query($args);
+
+	//The loop
+	if ($the_query->have_posts()) {
+		$result .= '<div class="projects">';
+		while ($the_query->have_posts()) {
+			$the_query->the_post();
+		}
+		$result .= '</div>';
+	}
+
+	wp_reset_postdata();
+
+	return $result;
+}
+
  ?>
