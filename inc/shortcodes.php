2d08ccb788d4358b76f2ffb15b3de183ebeb764f
Ghislain Arseneau <ghislain.arseneau@gmail.com>
2017-09-04 10:54:21 -0300

    Styling the shortcode to look more like a black book.

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -24,12 +24,19 @@ function gabuild_projects_shortcode($atts, $content = null) {
 	//The loop
 	if ($the_query->have_posts()) {
 		$result .= '<div class="gashortcode">';
-		$result .= '<h3 class="center">'. __('My Projects', 'gabuild') . '</h3>';
+		$result .= '<h3 class="center">'. __('Latest projects', 'gabuild') . '</h3>';
 		$result .= '<div class="projects grid-x grid-margin-x grid-padding-x medium-up-2 large-up-3">';
 		while ($the_query->have_posts()) {
 			$the_query->the_post();
 			$result .= '<div class="gaproject cell">';
 			$result .= '<h4>' . get_the_title() . '</h3>';
+			$result .= '<div class="gaproject-featured-img">';
+			if (has_post_thumbnail()){
+				$result .= get_the_post_thumbnail( null, 'post-thumbnail');
+			} else {
+				$result .= '<div class="thumbnail-filler"></div>';
+			}
+			$result .= '</div>';
 			$result .= '<div class="gaproject-content">';
 			$result .= get_the_excerpt();
 			$result .= '</div>';
