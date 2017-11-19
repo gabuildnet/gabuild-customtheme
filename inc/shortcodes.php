f6fb033ad81e1ff8dd881deb216d512bbbebf1b2
Ghislain Arseneau <ghislain.arseneau@gmail.com>
2017-11-19 16:06:14 -0400

    Changes to style

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -25,7 +25,7 @@ function gabuild_projects_shortcode($atts, $content = null) {
 	if ($the_query->have_posts()) {
 		$result .= '<div class="gashortcode">';
 		$result .= '<h3 class="center">'. __('Latest projects', 'gabuild') . '</h3>';
-		$result .= '<div class="projects grid-x grid-margin-x grid-padding-x medium-up-2 large-up-3">';
+		$result .= '<div class="projects grid-x grid-margin-x grid-padding-x grid-padding-y grid-margin-y medium-up-2 large-up-3">';
 		while ($the_query->have_posts()) {
 			$the_query->the_post();
 			$result .= '<div class="gaproject cell">';
